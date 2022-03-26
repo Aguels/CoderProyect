e25e66b04f38cedcc58ea40ef47edb6e99a04a84
Aguels <agustin.ariel.ambrosi@gmail.com>
2022-03-26 12:35:12 -0300

    Actualización directorio raíz

--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+import Musicy.views as vw
 
 urlpatterns = [
+    path("", vw.home),    
     path('admin/', admin.site.urls),
     path("musicy/", include("Musicy.urls"))
 ]
